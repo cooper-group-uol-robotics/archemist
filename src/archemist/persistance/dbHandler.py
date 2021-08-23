65ab9baf674de52429ca58c66ce2bc01bf5cfea8
Jakub Glowacki <jakubglowacki@mail.com>
2021-08-23 15:32:12 +0100

    adding db handling, sync

--- src/archemist/persistance/dbHandler.py
+++ src/archemist/persistance/dbHandler.py
@@ -1,5 +1,25 @@
-
-
+from pymongo import MongoClient
+import persistance.fsHandler
+import os
 class dbHandler:
     def __init__(self):
-        self.mongoURL = ""
+        self.client = MongoClient("mongodb://localhost:27017")
+        self.db=self.client.admin
+        print("Connected, Host: " + self.db.command("serverStatus")["host"])
+    
+    def syncConfig(self):
+        __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        handler = persistance.fsHandler.FSHandler()
+        db = self.client.config
+        if (db.workflowConfig.count_documents({"workflow": {"$exists": True}}) > 0):
+            db.workflowConfig.replace_one({"workflow": {"$exists": True}}, handler.loadYamlFile(os.path.join(__location__, 'config.yaml')))
+            return True
+        else:
+            db.workflowConfig.insert_one(handler.loadYamlFile(os.path.join(__location__, 'config.yaml')))
+            return False
+
+
+
+
+
