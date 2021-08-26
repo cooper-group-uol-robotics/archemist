c1b96fa8519ba9f65930084bd19e3f461f4e2195
Yak-28 <jakubglowacki@mail.com>
2021-08-26 15:37:25 +0100

    bugfixes

--- src/archemist/persistence/dbHandler.py
+++ src/archemist/persistence/dbHandler.py
@@ -10,62 +10,8 @@ class dbHandler:
         self.db = self.client.admin
         print("Connected, Host: " + self.db.command("serverStatus")["host"])
 
-    def changeStationProperty(self, station: str, newProperty, newValue):
-        db = self.client.config
-        conf = db.workflowConfig.find_one({"workflow": {"$exists": True}})
-        if (station in conf["workflow"]["Stations"]):
-            if (newProperty in conf["workflow"]["Stations"][station]):
-                oldValue = conf["workflow"]["Stations"][station][newProperty]
-                conf["workflow"]["Stations"][station][newProperty] = newValue
-                db.workflowConfig.replace_one(
-                    {"workflow": {"$exists": True}}, conf)
-                print("Success: Property " + str(newProperty) + " in station " + str(station) +
-                      " successfully changed from " + str(oldValue) + " to " + str(newValue))
-                return True
-            else:
-                print("Error: Property not found in station")
-                return False
-        else:
-            print("Error: Station not found in config")
-            return False
-
-    def changeLiquidProperty(self, liquid: str, newProperty, newValue):
-        b = self.client.config
-        conf = db.workflowConfig.find_one({"workflow": {"$exists": True}})
-        if (liquid in conf["workflow"]["Materials"]["Liquids"]):
-            if (newProperty in conf["workflow"]["Materials"]["Liquids"][liquid]):
-                oldValue = conf["workflow"]["Materials"]["Liquids"][liquid][newProperty]
-                conf["workflow"]["Materials"]["Liquids"][liquid][newProperty] = newValue
-                db.workflowConfig.replace_one(
-                    {"workflow": {"$exists": True}}, conf)
-                print("Success: Property " + str(newProperty) + " in liquid " + str(liquid) +
-                      " successfully changed from " + str(oldValue) + " to " + str(newValue))
-                return True
-            else:
-                print("Error: Property not found in station")
-                return False
-        else:
-            print("Error: Station not found in config")
-            return False
-
-    def changeSolidProperty(self, solid: str, newProperty, newValue):
-        b = self.client.config
-        conf = db.workflowConfig.find_one({"workflow": {"$exists": True}})
-        if (liquid in conf["workflow"]["Materials"]["Solids"]):
-            if (newProperty in conf["workflow"]["Materials"]["Solids"][solid]):
-                oldValue = conf["workflow"]["Materials"]["Solids"][solid][newProperty]
-                conf["workflow"]["Materials"]["Solids"][solid][newProperty] = newValue
-                db.workflowConfig.replace_one(
-                    {"workflow": {"$exists": True}}, conf)
-                print("Success: Property " + str(newProperty) + " in solid " + str(solid) +
-                      " successfully changed from " + str(oldValue) + " to " + str(newValue))
-                return True
-            else:
-                print("Error: Property not found in station")
-                return False
-        else:
-            print("Error: Station not found in config")
-            return False
+    def getDBAccess(self):
+        return self.client
 
     def importConfig(self):
         __location__ = os.path.realpath(
--- src/archemist/persistence/persistenceManager.py
+++ src/archemist/persistence/persistenceManager.py
@@ -2,6 +2,7 @@ import persistence.dbHandler
 import persistence.fsHandler
 import persistence.yParser
 import os
+from datetime import datetime
 
 
 class persistenceManager:
@@ -10,6 +11,70 @@ class persistenceManager:
         self.fshandler = persistence.fsHandler.FSHandler()
         self.parser = persistence.yParser.Parser()
         self.loadedConfig = None
+        self.client = self.dbhandler.getDBAccess()
+
+    def changeStationProperty(self, station: str, newProperty, newValue):
+        db = self.client.config
+        conf = db.workflowConfig.find_one({"workflow": {"$exists": True}})
+        if (station in conf["workflow"]["Stations"]):
+            if (newProperty in conf["workflow"]["Stations"][station]):
+                oldValue = conf["workflow"]["Stations"][station][newProperty]
+                conf["workflow"]["Stations"][station][newProperty] = newValue
+                conf["workflow"]["timestamp"] = datetime.now().strftime(
+                    "%m/%d/%Y, %H:%M:%S")
+                db.workflowConfig.replace_one(
+                    {"workflow": {"$exists": True}}, conf)
+                print("Success: Property " + str(newProperty) + " in station " + str(station) +
+                      " successfully changed from " + str(oldValue) + " to " + str(newValue))
+                return True
+            else:
+                print("Error: Property not found in station")
+                return False
+        else:
+            print("Error: Station not found in config")
+            return False
+
+    def changeLiquidProperty(self, liquid: str, newProperty, newValue):
+        db = self.client.config
+        conf = db.workflowConfig.find_one({"workflow": {"$exists": True}})
+        if (liquid in conf["workflow"]["Materials"]["liquids"]):
+            if (newProperty in conf["workflow"]["Materials"]["liquids"][liquid]):
+                oldValue = conf["workflow"]["Materials"]["liquids"][liquid][newProperty]
+                conf["workflow"]["Materials"]["liquids"][liquid][newProperty] = newValue
+                conf["workflow"]["timestamp"] = datetime.now().strftime(
+                    "%m/%d/%Y, %H:%M:%S")
+                db.workflowConfig.replace_one(
+                    {"workflow": {"$exists": True}}, conf)
+                print("Success: Property " + str(newProperty) + " in liquid " + str(liquid) +
+                      " successfully changed from " + str(oldValue) + " to " + str(newValue))
+                return True
+            else:
+                print("Error: Property not found in station")
+                return False
+        else:
+            print("Error: Station not found in config")
+            return False
+
+    def changeSolidProperty(self, solid: str, newProperty, newValue):
+        db = self.client.config
+        conf = db.workflowConfig.find_one({"workflow": {"$exists": True}})
+        if (liquid in conf["workflow"]["Materials"]["solids"]):
+            if (newProperty in conf["workflow"]["Materials"]["solids"][solid]):
+                oldValue = conf["workflow"]["Materials"]["solids"][solid][newProperty]
+                conf["workflow"]["Materials"]["solids"][solid][newProperty] = newValue
+                conf["workflow"]["timestamp"] = datetime.now().strftime(
+                    "%m/%d/%Y, %H:%M:%S")
+                db.workflowConfig.replace_one(
+                    {"workflow": {"$exists": True}}, conf)
+                print("Success: Property " + str(newProperty) + " in solid " + str(solid) +
+                      " successfully changed from " + str(oldValue) + " to " + str(newValue))
+                return True
+            else:
+                print("Error: Property not found in station")
+                return False
+        else:
+            print("Error: Station not found in config")
+            return False
 
     def loadConfig(self):
         self.dbhandler.importConfig()
