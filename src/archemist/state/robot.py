1ed5c8bfe5c78b15c21530bf858863cbd66ab4ad
Jakub Glowacki <jakubglowacki@mail.com>
2021-09-01 09:45:02 +0100

    robot class(es) and station/robot implementation folders

--- src/archemist/state/robot.py
+++ src/archemist/state/robot.py
@@ -1,1 +1,12 @@
-# class RoboChemist():
+class robot:
+    def __init__(self, name: str, id: int):
+        self._name = name
+        self._id = id
+
+class mobileRobot(robot):
+    def __init__(self, name: str, id: int):
+        super().__init__(name, id)
+
+class armRobot(robot):
+    def __init__(self, name: str, id: int):
+        super().__init__(name, id)
