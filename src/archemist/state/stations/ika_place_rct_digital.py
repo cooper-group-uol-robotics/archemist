071326e3f40390b068d7bacf9626bde7ed395370
Yak-28 <jakubglowacki@mail.com>
2021-09-07 11:45:39 +0100

    beginning of station handling

--- src/archemist/state/stations/ika_place_rct_digital.py
+++ src/archemist/state/stations/ika_place_rct_digital.py
@@ -1,6 +1,10 @@
 from src.archemist.state.station import Station, Location
-
+from src.archemist.util.rosMsgCoder import rosMsgCoder
 
 class ika_plate_rct_digital(Station):
     def __init__(self, name: str, id: int, loc: Location):
         super().__init__(name, id, loc)
+
+class ikaOpDescriptor(StationOpDescriptor):
+    def __init__(self):
+        super().__init__()
--- src/archemist/state/stations/solid_dispensing_quantos_QS3.py
+++ src/archemist/state/stations/solid_dispensing_quantos_QS3.py
@@ -4,3 +4,4 @@ from src.archemist.state.station import Station, Location
 class solid_dispensing_quantos_QS3(Station):
     def __init__(self, name: str, id: int, loc: Location):
         super().__init__(name, id, loc)
+        self.carouselPos = 0
