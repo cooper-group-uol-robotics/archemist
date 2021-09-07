3896d12f9603c6f1daa4f23c65a01803ac54a147
Yak-28 <jakubglowacki@mail.com>
2021-09-07 14:33:07 +0100

    state changes

--- src/archemist/state/stations/ika_place_rct_digital.py
+++ src/archemist/state/stations/ika_place_rct_digital.py
@@ -1,9 +1,22 @@
 from src.archemist.state.station import Station, Location
 from src.archemist.util.rosMsgCoder import rosMsgCoder
+from enum import Enum
 
 class ika_plate_rct_digital(Station):
     def __init__(self, name: str, id: int, loc: Location):
         super().__init__(name, id, loc)
+        self._currentTemperature = None
+        self._setTemperature = None
+        self._stirringSpeed = None
+        self._externalTemp = None
+        self._viscosityTrend = None
+
+    def readDescriptor(self, descriptor):
+        self._currentTemperature = descriptor[0]
+        self._setTemperature = descriptor[1]
+        self._stirringSpeed = descriptor[2]
+        self._externalTemp = descriptor[3]
+        self._viscosityTrend = descriptor[4]
 
 class ikaOpDescriptor(StationOpDescriptor):
     def __init__(self):
