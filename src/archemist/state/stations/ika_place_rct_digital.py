4d38cb145f88001b498a4a57d385c3e1fcf824e8
stoic-roboticist <hatem.fakhruldeen@gmail.com>
2021-09-08 10:13:45 +0100

    Merge branch 'master' of https://github.com/cooper-group-uol-robotics/archemist

--- src/archemist/state/stations/ika_place_rct_digital.py
+++ src/archemist/state/stations/ika_place_rct_digital.py
@@ -2,21 +2,91 @@ from src.archemist.state.station import Station, Location
 from src.archemist.util.rosMsgCoder import rosMsgCoder
 from enum import Enum
 
-class ikaState(Enum):
-    STOPPED = 0
+
+class ikaMode(Enum):
     HEATING = 1
     STIRRING = 2
     HEATINGSTIRRING = 3
+
 
 class ika_plate_rct_digital(Station):
     def __init__(self, name: str, id: int, loc: Location):
         super().__init__(name, id, loc)
         self._currentTemperature = None
         self._setTemperature = None
-        self._stirringSpeed = None
+        self._currentStirringSpeed = None
+        self._setStirringSpeed = None
         self._externalTemp = None
         self._viscosityTrend = None
-        self._state = ikaState.STOPPED
+        self._mode = None
+        self._executing = False
+
+    @property
+    def currentTemperature(self):
+        return self._currentTemperature
+
+    @currentTemperature.setter
+    def currentTemperature(self, value):
+        self._currentTemperature = value
+
+    @property
+    def setTemperature(self):
+        return self._setTemperature
+
+    @setTemperature.setter
+    def setTemperature(self, value):
+        self._setTemperature = value
+
+    @property
+    def currentStirringSpeed(self):
+        return self._currentStirringSpeed
+
+    @currentStirringSpeed.setter
+    def currentStirringSpeed(self, value):
+        self._currentStirringSpeed = value
+
+    @property
+    def setStirringSpeed(self):
+        return self._setStirringSpeed
+
+    @setStirringSpeed.setter
+    def setStirringSpeed(self, value):
+        self._setStirringSpeed = value
+
+    @property
+    def externalTemp(self):
+        return self._externalTemp
+
+    @externalTemp.setter
+    def externalTemp(self, value):
+        self._externalTemp = value
+
+    @property
+    def viscosityTrend(self):
+        return self._viscosityTrend
+
+    @viscosityTrend.setter
+    def viscosityTrend(self, value):
+        self._viscosityTrend = value
+
+    @property
+    def mode(self):
+        return self._mode
+
+    @mode.setter
+    def mode(self, value):
+        if isinstance(value, ikaMode):
+            self._mode = value
+        else:
+            raise ValueError
+
+    @property
+    def executing(self):
+        return self._executing
+
+    @executing.setter
+    def executing(self, value):
+        self._executing = value
 
     def readDescriptor(self, descriptor):
         self._currentTemperature = descriptor[0]
@@ -24,7 +94,6 @@ class ika_plate_rct_digital(Station):
         self._stirringSpeed = descriptor[2]
         self._externalTemp = descriptor[3]
         self._viscosityTrend = descriptor[4]
-        self._state = descriptor[5]
 
 class ikaOpDescriptor(StationOpDescriptor):
     def __init__(self):
