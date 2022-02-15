50054f5dacb18de6563f6016d2bedd899385936a
stoic-roboticist <hatem.fakhruldeen@gmail.com>
2022-02-15 15:27:36 +0000

    refactored panda handler.

--- src/archemist/processing/robotHandlers/pandaFranka_handler.py
+++ src/archemist/processing/robotHandlers/pandaFranka_handler.py
@@ -1,27 +1,29 @@
-from archemist.state.robot import Robot
+from archemist.state.robot import Robot, MoveSampleOp
 import rospy
-from archemist_msgs.msg import HandlerBusMessage, PandaCommand
+from franka_msgs_archemist.msg import PandaTask, TaskStatus
 from kmriiwa_chemist_msgs.msg import TaskStatus
-from archemist.util.rosMsgCoder import rosMsgCoder
-from archemist.state.robots import PandaFranka
-from archemist.state.state import State
-from archemist.util.location import Location
 from archemist.processing.handler import RobotHandler
-from rospy.core import is_shutdown
 
-class PandaHandler(RobotHandler):
+class PandaFranka_Handler(RobotHandler):
     def __init__(self, robot: Robot):
         super().__init__(robot)
         rospy.init_node( f'{self._robot}_handler')
-        self._pandaCmdPub = rospy.Publisher('/panda1/commands', PandaCommand, queue_size=1)
+        self._pandaCmdPub = rospy.Publisher('/panda1/commands', PandaTask, queue_size=1)
         rospy.Subscriber('/panda1/task_status', TaskStatus, self.panda_task_cb, queue_size=2)
         self._panda_task = ''
         self._panda_done = False
+        self._task_counter = 0
 
-        rospy.loginfo(f'{self._robot}_handler is running')
-        while (not rospy.is_shutdown()):
-            self.handle()
-            rospy.sleep(3)
+    def run(self):
+        try:
+            rospy.loginfo(f'{self._robot}_handler is running')
+            while (not rospy.is_shutdown()):
+                self.handle()
+                rospy.sleep(3)
+        except KeyboardInterrupt:
+            rospy.loginfo(f'{self._robot}_handler is terminating!!!')
+
+
 
     def panda_task_cb(self, msg):
         if msg.task_name == self._panda_task and msg.task_state == TaskStatus.FINISHED:
@@ -33,31 +35,22 @@ class PandaHandler(RobotHandler):
             rospy.sleep(0.1)
         self._panda_done = False
 
-    def process_op(self, robotOp):
-        if robotOp.start_pos.frame_name == 'handover_cube' and robotOp.end_pos.frame_name == 'pump':
-            return PandaCommand(panda_command=PandaCommand.MOVEVIALINITIALPOSITIONTOPUMP)
-        elif robotOp.start_pos.frame_name == 'pump' and robotOp.end_pos.frame_name == 'ika':
-            return PandaCommand(panda_command=PandaCommand.MOVEVIALPUMPTOSTIRRER)
-        elif robotOp.start_pos.frame_name == 'ika' and robotOp.end_pos.frame_name == 'handover_cube':
-            return PandaCommand(panda_command=PandaCommand.MOVEVIALSTIRRERTOINITIALPOSITION)
-        elif robotOp.start_pos.frame_name == 'ika' and robotOp.end_pos.frame_name == 'scale':
-            return PandaCommand(panda_command=PandaCommand.MOVEVIALHEATERTOSCALE)
-        elif robotOp.start_pos.frame_name == 'scale' and robotOp.end_pos.frame_name == 'ika':
-            return PandaCommand(panda_command=PandaCommand.MOVEVIALSCALETOHEATER)
-        elif robotOp.start_pos.frame_name == 'scale' and robotOp.end_pos.frame_name == 'handover_cube':
-            return PandaCommand(panda_command=PandaCommand.MOVEVIALSCALETOINITIALPOSITION)
-        elif robotOp.start_pos.frame_name == 'pump' and robotOp.end_pos.frame_name == 'scale':
-            return PandaCommand(panda_command=PandaCommand.MOVEVIALPUMPTOSCALE)
+    def _process_op(self, robotOp):
+        if isinstance(robotOp, MoveSampleOp):
+            return PandaTask(task_type=PandaTask.MOVE_VIAL_TASK, 
+                             task_name=f'MOVE_VIAL_TASK_{self._task_counter}', 
+                             start_frame=robotOp.start_location.frame_name,
+                             target_frame=robotOp.target_location.frame_name)
         else:
-            rospy.logerr('unknown panda op')    
+            rospy.logerr('unknown robot op')    
         return None
 
     def execute_job(self):
         station_robot_job = self._robot.assigned_job
         station_robot_job.robot_op.add_timestamp()
         # this has to be changed to convert vial job to a good message to panda
-        pandaJob = self.process_op(station_robot_job.robot_op)
-        self._panda_task = str(pandaJob.panda_command)
+        pandaJob = self._process_op(station_robot_job.robot_op)
+        self._panda_task = pandaJob.task_name
         rospy.loginfo('executing ' + self._panda_task)
         self._pandaCmdPub.publish(pandaJob)
         self.wait_for_panda()
@@ -65,10 +58,10 @@ class PandaHandler(RobotHandler):
         station_robot_job.robot_op.output.has_result = True
         station_robot_job.robot_op.output.success = True
         station_robot_job.robot_op.output.add_timestamp()
-
+        self._task_counter =+ 1
         return station_robot_job
 
 
 
-if __name__ == '__main__':
-    panad_handler = PandaHandler(1)
+# if __name__ == '__main__':
+#     panad_handler = PandaHandler(1)
