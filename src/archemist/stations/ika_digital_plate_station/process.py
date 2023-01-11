583d55ed5edb464326443dbe049bf12e3082052c
sgalnunt <sgalnunt@liverpool.ac.uk>
2023-01-11 16:21:49 +0000

    updated state machine and yumi handler

--- src/archemist/stations/ika_digital_plate_station/process.py
+++ src/archemist/stations/ika_digital_plate_station/process.py
@@ -1,6 +1,8 @@
 from transitions import State
 from archemist.core.persistence.object_factory import StationFactory
+from archemist.core.state.robot import RobotTaskType
 from archemist.robots.yumi_robot.state import YuMiRobotTask
+from archemist.robots.kmriiwa_robot.state import KukaLBRTask
 from archemist.core.processing.station_process_fsm import StationProcessFSM
 from archemist.core.state.station import Station
 from typing import Dict
@@ -15,18 +17,34 @@ class IKAStirPlateSm(StationProcessFSM):
         self._current_batch_index = 0
 
         ''' States '''
-        states = [ State(name='init_state', on_enter='_print_state'), 
+        states = [ State(name='init_state', on_enter='_print_state'),
+            State(name='place_8_well_rack', on_enter=['request_8_well_rack', '_print_state']),
+            State(name='place_pxrd_rack', on_enter=['request_pxrd_rack', '_print_state']),
             State(name='load_stir_plate', on_enter=['request_load_stir_plate', '_print_state']),
             State(name='stir', on_enter=['request_stir_op', '_print_state']),
             State(name='final_state', on_enter=['finalize_batch_processing', '_print_state'])]
 
         transitions = [
-            {'trigger':self._trigger_function, 'source':'init_state', 'dest': 'load_stir_plate', 'conditions':'all_batches_assigned'},
+            {'trigger':self._trigger_function, 'source':'init_state', 'dest': 'place_8_well_rack', 'conditions':'all_batches_assigned'},
+            {'trigger':self._trigger_function, 'source':'place_8_well_rack', 'dest': 'place_pxrd_rack', 'conditions':'is_station_job_ready'},
+            {'trigger':self._trigger_function, 'source':'place_pxrd_rack', 'dest': 'load_stir_plate', 'conditions':'is_station_job_ready'},
             {'trigger':self._trigger_function,'source':'load_stir_plate','dest':'stir', 'conditions':'is_station_job_ready'},
             {'trigger':self._trigger_function,'source':'stir','dest':'final_state', 'conditions':'is_station_job_ready'},
         ]
 
         self.init_state_machine(states=states, transitions=transitions)
+
+    def request_8_well_rack(self):
+        robot_job = KukaLBRTask.from_args(name='LoadEightWRackYumiStation',params=[True], 
+                                            type=RobotTaskType.UNLOAD_FROM_ROBOT, location=self._station.location)
+        current_batch_id = self._station.assigned_batches[self._current_batch_index].id
+        self._station.request_robot_op(robot_job,current_batch_id)
+
+    def request_pxrd_rack(self):
+        robot_job = KukaLBRTask.from_args(name='LoadPXRDRackYumiStation',params=[False], 
+                                            type=RobotTaskType.MANIPULATION, location=self._station.location)
+        current_batch_id = self._station.assigned_batches[self._current_batch_index].id
+        self._station.request_robot_op(robot_job,current_batch_id)
 
     def request_load_stir_plate(self):
         robot_job = YuMiRobotTask.from_args(name='loadIKAPlate', location=self._station.location)
--- src/archemist/stations/shaker_plate_station/process.py
+++ src/archemist/stations/shaker_plate_station/process.py
@@ -1,6 +1,8 @@
 from transitions import State
 from archemist.core.persistence.object_factory import StationFactory
 from archemist.robots.yumi_robot.state import YuMiRobotTask
+from archemist.robots.kmriiwa_robot.state import KukaLBRTask
+from archemist.core.state.robot import RobotTaskType
 from archemist.core.processing.station_process_fsm import StationProcessFSM
 from archemist.core.state.station import Station
 from typing import Dict
@@ -19,6 +21,7 @@ class YumiShakerPlateSm(StationProcessFSM):
             State(name='shake', on_enter=['request_shake_op', '_print_state']),
             State(name='unload_shaker_plate', on_enter=['request_unload_shaker_plate', '_print_state']),
             State(name='unscrew_caps', on_enter=['request_unscrew_caps', '_print_state']),
+            State(name='pick_pxrd_rack', on_enter=['request_pxrd_rack_pickup', '_print_state']),
             State(name='final_state', on_enter=['finalize_batch_processing', '_print_state'])]
 
         transitions = [
@@ -26,7 +29,8 @@ class YumiShakerPlateSm(StationProcessFSM):
             {'trigger':self._trigger_function,'source':'load_shaker_plate','dest':'shake', 'conditions':'is_station_job_ready'},
             {'trigger':self._trigger_function,'source':'shake','dest':'unload_shaker_plate', 'conditions':'is_station_job_ready'},
             {'trigger':self._trigger_function,'source':'unload_shaker_plate','dest':'unscrew_caps', 'conditions':'is_station_job_ready'},
-            {'trigger':self._trigger_function, 'source':'unscrew_caps','dest':'final_state', 'conditions':'is_station_job_ready'}
+            {'trigger':self._trigger_function,'source':'unscrew_caps','dest':'pick_pxrd_rack', 'conditions':'is_station_job_ready'},
+            {'trigger':self._trigger_function, 'source':'pick_pxrd_rack','dest':'final_state', 'conditions':'is_station_job_ready'}
         ]
 
         self.init_state_machine(states=states, transitions=transitions)
@@ -50,6 +54,12 @@ class YumiShakerPlateSm(StationProcessFSM):
         robot_job = YuMiRobotTask.from_args(name='unscrewCaps', location=self._station.location)
         current_batch_id = self._station.assigned_batches[self._current_batch_index].id
         self._station.request_robot_op(robot_job, current_batch_id)
+
+    def request_pxrd_rack_pickup(self):
+        robot_job = KukaLBRTask.from_args(name='UnloadPXRDRackYumiStation',params=[True], 
+                                            type=RobotTaskType.LOAD_TO_ROBOT, location=self._station.location)
+        current_batch_id = self._station.assigned_batches[self._current_batch_index].id
+        self._station.request_robot_op(robot_job,current_batch_id)
 
     def finalize_batch_processing(self):
         self._station.process_assigned_batches()
