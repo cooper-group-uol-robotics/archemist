0b76a118a70ac4e117a0421aec3e8232c3ffae9a
Satheeshkumar <sathiz.v@gmail.com>
2023-03-15 18:58:05 +0000

    que_racks_timer and waiting states added
    threading created for waiting timer function, function developed for the waiting state to check the status of current processing batches.

--- src/archemist/stations/waiting_station/process.py
+++ src/archemist/stations/waiting_station/process.py
@@ -11,6 +11,34 @@ from archemist.core.processing.station_process_fsm import StationProcessFSM
 from archemist.core.persistence.object_factory import StationFactory
 from archemist.core.util import Location
 
+import threading
+from threading import Thread
+import time
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-9s) %(message)s',)
+
+
+
+class WaitingTimer():
+    def __init__(self,batch_index):
+        self._timer = {}
+        self._timer['id'] = batch_index
+        self._timer['waiting_left'] = 0
+        self._timer['waiting_done'] = False
+        self._t= threading.Timer(20, self.thread_complete)
+        self._t.start()
+        print('Waiting Started')
+        
+    def thread_complete(self):
+        self._timer['waiting_done'] = True
+        print(f'waiting of batch {self._timer[0]} is done')
+
+    def waiting_completed(self) ->bool:
+        return not self._t.is_alive()
+
+    
 class WaitingStationSm(StationProcessFSM):
     
     def __init__(self, station: Station, params_dict: Dict):
@@ -19,6 +47,13 @@ class WaitingStationSm(StationProcessFSM):
             self._status['operation_complete'] = False
         self._status['batch_index'] = 0
         self._status['batches_count'] = 0
+        if 'batch_waiting' not in self._status.keys():
+            self._status['batch_waiting'] = 0
+        if 'no_racks_assigned' not in self._status.keys():
+            self._status['no_racks_assigned'] = False
+        self._status['_current_batch_timing'] = []
+        self._t = []
+
         
         # if '_current_batch_capacity' not in self._status.keys():
         #     self._status['_current_batch_capacity'] = 3
@@ -27,12 +62,12 @@ class WaitingStationSm(StationProcessFSM):
         # if '_station_batches_occupied' not in self._status.keys():
         #     self._status['_station_batches_occupied'] = 0
 
-
         ''' States '''
         states = [ State(name='init_state'), 
             State(name='disable_auto_functions', on_enter=['request_disable_auto_functions']),
             State(name='enable_auto_functions', on_enter=['request_enable_auto_functions']),
-            State(name='waiting_process', on_enter=['request_process_operation']),
+            State(name='queue_racks_timer', on_enter=['request_process_operation']),
+            State(name='waiting', on_enter=['check_loaded_racks']),
             State(name='load_batch', on_enter=['request_load_batch']),
             State(name='added_batch_update', on_enter=['update_loaded_batch']),
             State(name='unload_batch', on_enter=['request_unload_batch']),
@@ -46,31 +81,53 @@ class WaitingStationSm(StationProcessFSM):
             {'trigger':self._trigger_function, 'source':'load_batch','dest':'added_batch_update', 'conditions':'is_station_job_ready'},
             {'trigger':self._trigger_function, 'source':'added_batch_update','dest':'load_batch', 'unless':'are_all_batches_loaded', 'conditions':'is_station_job_ready'},
             {'trigger':self._trigger_function, 'source':'added_batch_update','dest':'enable_auto_functions', 'conditions':['is_station_job_ready','are_all_batches_loaded']},
-            {'trigger':self._trigger_function, 'source':'enable_auto_functions','dest':'waiting_process', 'unless':'is_station_operation_complete', 'conditions':['is_station_job_ready','are_all_batches_loaded']},
-            {'trigger':self._trigger_function, 'source':'waiting_process','dest':'disable_auto_functions', 'conditions':'is_station_job_ready', 'before':'process_batches'},
+            {'trigger':self._trigger_function, 'source':'enable_auto_functions','dest':'queue_racks_timer', 'unless':'is_station_operation_complete', 'conditions':['is_station_job_ready','are_all_batches_loaded']},
+            {'trigger':self._trigger_function, 'source':'queue_racks_timer','dest':'waiting', 'unless':'is_station_operation_complete', 'conditions':['is_station_job_ready','are_all_batches_loaded']},
+            {'trigger':self._trigger_function, 'source':'waiting','dest':'disable_auto_functions', 'conditions':'is_station_job_ready', 'before':'process_batches'},
             {'trigger':self._trigger_function, 'source':'disable_auto_functions','dest':'unload_batch','conditions':['is_station_operation_complete','is_station_job_ready']},
             {'trigger':self._trigger_function, 'source':'unload_batch','dest':'removed_batch_update', 'conditions':'is_station_job_ready'},
             {'trigger':self._trigger_function, 'source':'removed_batch_update','dest':'unload_batch', 'unless':'are_all_batches_unloaded', 'conditions':'is_station_job_ready'},
             {'trigger':self._trigger_function, 'source':'removed_batch_update','dest':'enable_auto_functions', 'conditions':['is_station_job_ready','are_all_batches_unloaded']},
-            {'trigger':self._trigger_function, 'source':'enable_auto_functions','dest':'final_state', 'conditions':['is_station_job_ready','is_station_operation_complete']}
+            {'trigger':self._trigger_function, 'source':'enable_auto_functions','dest':'waiting', 'conditions':['is_station_job_ready','are_all_batches_unloaded']},
+            {'trigger':self._trigger_function, 'source':'waiting','dest':'disable_auto_functions', 'conditions':['is_station_job_ready', 'new_racks_assigned']},
+            {'trigger':self._trigger_function, 'source':'disable_auto_functions','dest':'load_batch', 'unless':'is_station_operation_complete','conditions':'is_station_job_ready'},
+            {'trigger':self._trigger_function, 'source':'waiting','dest':'final_state', 'conditions':['is_station_job_ready','is_station_operation_complete','are_no_racks_waiting','are_no_racks_assigned']}
         ]   
 
         self.init_state_machine(states=states, transitions=transitions)
 
     def is_station_operation_complete(self):
         return self._status['operation_complete']
+    
+    def are_no_racks_waiting(self) ->bool:
+        if self._status['batch_waiting'] == 0:
+            return True
+        else:
+            return False
+        
+    def new_racks_assigned(self) ->bool:
+        return True
+
+    def are_no_racks_assigned(self) -> bool:
+        return 
 
     def request_load_batch(self):
         robot_job = KukaLBRTask.from_args(name='LoadWaitingStation',params=[True,self._status['batch_index']+1], 
                                             type=RobotTaskType.UNLOAD_FROM_ROBOT, location=self._station.location)
         current_batch_id = self._station.assigned_batches[self._status['batch_index']].id
         self._station.request_robot_op(robot_job,current_batch_id)
+        self._status['batch_waiting'] += 1
+        self.batch_index = self._status['batch_index']
+        self._t.insert(self.batch_index, f'batch_{self.batch_index}')
 
     def request_unload_batch(self):
         robot_job = KukaLBRTask.from_args(name='UnloadWaitingStation',params=[False,self._status['batch_index']+1],
                                 type=RobotTaskType.LOAD_TO_ROBOT, location=self._station.location)
         current_batch_id = self._station.assigned_batches[self._status['batch_index']].id
         self._station.request_robot_op(robot_job,current_batch_id)
+        self._status['batch_waiting'] -= 1
+        self.batch_index = self._status['batch_index']
+        self._t.pop(self.batch_index)
         
     def request_disable_auto_functions(self):
         self._station.request_robot_op(KukaLBRMaintenanceTask.from_args('DiableAutoFunctions',[False]))
@@ -79,10 +136,22 @@ class WaitingStationSm(StationProcessFSM):
         self._station.request_robot_op(KukaLBRMaintenanceTask.from_args('EnableAutoFunctions',[False]))
 
     def request_process_operation(self):
-        current_op = self._station.assigned_batches[-1].recipe.get_current_task_op()
-        self._station.assign_station_op(current_op)
+        # current_op = self._station.assigned_batches[-1].recipe.get_current_task_op()
+        # self._station.assign_station_op(current_op)
+       
+        self._t[self.batch_index-1] =  WaitingTimer(self.batch_index)
 
-        
+    def check_loaded_racks(self):
+        self._thread = Thread(target=self._mock_execution)
+        self._thread.start()
+
+    def _mock_execution(self):
+        for batch in self._t:
+            if self._t[batch]._timer['waiting_done']:
+                print(f'waiting of the batch {batch} is completed and unload them')
+            else:
+                pass
+    
     def process_batches(self):
         last_operation_op = self._station.station_op_history[-1]
         for batch in self._station.assigned_batches:
@@ -100,7 +169,6 @@ class WaitingStationSm(StationProcessFSM):
         # #     for _ in range(0, batch.num_samples):
         # #             batch.add_station_op_to_current_sample(last_operation_op)
         # #             batch.process_current_sample()
-
 
     def finalize_batch_processing(self):
         self._station.process_assigned_batches()
