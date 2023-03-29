04ac44d8f1955219784d786e99810d1b7ec564f4
stoic-roboticist <hatem.fakhruldeen@gmail.com>
2023-03-29 19:41:29 +0100

    Revert "pxrd station handler update"
    This reverts commit ee4194026ffcb20b8f793b71cb28a58fabfc14ef.

--- src/archemist/stations/pxrd_station/handler.py
+++ src/archemist/stations/pxrd_station/handler.py
@@ -4,25 +4,21 @@ from archemist.core.state.station import Station
 from .state import PXRDAnalysisOpDescriptor
 from archemist.core.processing.handler import StationHandler
 from std_msgs.msg import String
-from pxrd_msgs.msg import PxrdCommand, PxrdStatus
 import time
 from threading import Thread
 
-#### ACTUAL WORKFLOW (REAL SCANS + PXRD PYTHON DRIVER )
+
+##### TESTING WORKFLOW (FAKE SCANS)
 
 class PXRDStationROSHandler(StationHandler):
     def __init__(self, station: Station):
         super().__init__(station)
-        self._current_pxrd_status = PxrdStatus.NOT_LAUNCHED_YET
-        self._desired_pxrd_status = None
-        rospy.init_node(f'{self._station}_handler')
-        self.pub_pxrd = rospy.Publisher("/PXRD_commands", PxrdCommand, queue_size=1)
-        rospy.Subscriber('/PXRD_status', PxrdStatus, self._pxrd_state_update, queue_size=1)
-        rospy.sleep(1)
         self._thread = None
 
     def execute_op(self):
-        self._thread = Thread(target=self._real_execution)
+        current_op = self._station.get_assigned_station_op()
+        print(f'performing pxrd operation {current_op}')    
+        self._thread = Thread(target=self._mock_execution)
         self._thread.start()
 
     def is_op_execution_complete(self):
@@ -43,20 +39,63 @@ class PXRDStationROSHandler(StationHandler):
         except KeyboardInterrupt:
             print(f'{self._station}_handler is terminating!!!')
 
-    def _pxrd_state_update(self, msg):
-        if self._current_pxrd_status != msg.pxrd_status:
-            self._current_pxrd_status = msg.pxrd_status
-
-    def _real_execution(self):
-        current_op = self._station.get_assigned_station_op()
-        print(f'performing pxrd operation {current_op}')  
-        if (isinstance(current_op,PXRDAnalysisOpDescriptor)):
-            if (isinstance(self._current_pxrd_status,PxrdStatus.NOT_LAUNCHED_YET)):
-                rospy.loginfo('starting pxrd job')
-                for i in range(10):
-                    self.pub_pxrd.publish(PXRD_commands=PxrdCommand.EXECUTE)
-                self._desired_pxrd_status =  PxrdStatus.EXECUTION_DONE
-            elif (isinstance(self._current_pxrd_status,PxrdStatus.EXECUTION_DONE)):
-                for i in range(10):
-                    self.pub_pxrd.publish(PXRD_commands=PxrdCommand.TERMINATE)
+    def _mock_execution(self):
         time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+#### ACTUAL WORKFLOW (REAL SCANS + PXRD PYTHON DRIVER )
+#class PXRDStationROSHandler(StationHandler):
+#    def __init__(self, station: Station):
+#        super().__init__(station)
+#        self._current_cs_status = 'PXRDStatus.IDLE'
+#        self._desired_cs_status = None
+#        rospy.init_node(f'{self._station}_handler')
+#        self.PXRD_pub = rospy.Publisher("/PXRD/command", String, queue_size=1)
+#        rospy.Subscriber('/PXRD/status', String, self._cs_state_update, queue_size=1)
+#        rospy.sleep(1)
+        
+
+#    def run(self):
+#        rospy.loginfo(f'{self._station}_handler is running')
+#        try:
+#            while not rospy.is_shutdown():
+#                self.handle()
+#                rospy.sleep(2)
+#        except KeyboardInterrupt:
+#            rospy.loginfo(f'{self._station}_handler is terminating!!!')
+
+#   def execute_op(self):
+#        current_op = self._station.get_assigned_station_op()
+#        if (isinstance(current_op,PXRDAnalysisOpDescriptor)):
+#            rospy.loginfo('opening pxrd door')
+#            for i in range(10):
+#                self.PXRD_pub.publish('start_analysis')
+#            self._desired_cs_status = 'PXRDStatus.JOB_COMPLETE'
+#        else:
+#            rospy.logwarn(f'[{self.__class__.__name__}] Unknown operation was received')
+
+#    def is_op_execution_complete(self) -> bool:
+#        if self._desired_cs_status == self._current_cs_status:
+#            self._desired_cs_status = None
+#            return True
+#        else:
+#            return False
+
+#    def get_op_result(self) -> Tuple[bool, Dict]:
+#        if self._current_cs_status != 'pxrdStatus.ERROR':
+#            return True, {}
+ #       else:
+#            return False
+
+#    def _cs_state_update(self, msg):
+#        if self._current_cs_status != msg.data:
+#            self._current_cs_status = msg.data
