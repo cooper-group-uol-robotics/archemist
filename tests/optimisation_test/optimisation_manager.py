fbf36ef445aecb20d892b0ad7c384a76036b92f8
Satheeshkumar <sathiz.v@gmail.com>
2023-04-26 19:03:25 +0100

    some progress

--- tests/optimisation_test/optimisation_manager.py
+++ tests/optimisation_test/optimisation_manager.py
@@ -15,22 +15,21 @@ class State:
         model.save()
         return cls(model)
     
-class OptimisationManager(OptimizerBase):
+class OptimisationManager():
     
     def __init__(self,
                  config_file: str,
                  batch_size: int = 15,) -> None:
-        self.config = self._read_config(file_path=config_file)
-        self.batch_size = batch_size
-        bound = self.generate_bound_from_config()
-        self.component_keys = bound.keys() 
+        self._config_dict = YamlHandler.load_config_file(config_file)
+        self._batch_size = batch_size
+        bound = self._generate_bound_from_config()
+        self._component_keys = bound.keys() 
         #self.target_name = None  
-
-        self.model = self.generate_model(pbounds=bound)
+        self.model = self._generate_model(pbounds=bound)
         self._probed_points = pd.DataFrame([])
 
     def construct_optimiser_from_config_file(self, config_file_path:str):
-        return State.from_dict(self.config['optimizer'])
+        return State.from_dict(self._config_dict['optimizer'])
     
     def is_recipe_template_available(self, _template_path):
         _template_path = Path(_template_path)
@@ -39,21 +38,60 @@ class OptimisationManager(OptimizerBase):
 
     #optimization_part
     
-    def generate_bound_from_config():
-        pass
+    def _generate_bound_from_config(self):
+        constraint = {}
+        for key in self._config_dict['experiment']['components']:
+            constraint[key] = (self._config_dict['experiment']['components'][key]['lower_bound'],
+                               self._config_dict['experiment']['components'][key]['upper_bound'])
+        return constraint
 
-    def generate_model():
-        pass
+    def _generate_model(self):
+        """
+        Generate optimization model from config file based on the 'optimizer' part.
+        If the model requires constraint information, please pass it via kwargs.
+        """
+        module = importlib.import_module(self._config_dict['optimizer']['module'])
+        optimizer_class = getattr(module, self._config_dict['optimizer']['class'])
+        return optimizer_class(**self._config_dict['optimizer']['hyperparameters'], **kwargs)
+
 
     def update_model(self, data: pd.DataFrame, **kwargs):
-        pass
+        """
+        Record probed points and update optimization model.
+        :param data:
+
+        """
+        self._probed_points = self._probed_points.append(data)
+        params, targets = self.pandas_to_params_targets(data)
+        for param, target in zip(params, targets):
+            self.model.register(param, target)
 
     def pandas_to_params_targets(self, data: pd.DataFrame):
-        pass
+        """
+        Split experiment result dataframe into dictionary format params and target array.
+        """
+        # get optimization target name
+        if self.target_name is None:
+            target_name = [i for i in data.columns if i not in self.component_keys]
+            assert len(target_name) == 1, 'Require only one unseen column as optimization target'
+            self.target_name = target_name[0]
+        else:
+            assert self.target_name in data.columns, 'Optimization target not in result data, please check dataframe ' \
+                                                     'format.'
+
+        targets = data[self.target_name].to_numpy()
+        data = data.drop(columns=self.target_name)
+        params = []
+        for row_index in range(len(data)):
+            params.append(data.iloc[row_index].to_dict())
+        return params, targets
 
     def generate_batch(self, **kwargs) -> pd.DataFrame:
         pass
 
 
-
+if __name__ == "main":
+    test_data = pd.DataFrame([[3,4], [1, 2]], columns=['x', 'y'])
+    opt = OptimisationManager('test_config.yaml')
+    opt.update_model(test_data)
 
