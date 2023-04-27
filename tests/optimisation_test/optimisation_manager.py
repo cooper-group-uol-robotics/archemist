c6cc5ce81fa36849e8f77d50c05cea79628f410b
Satheeshkumar <sathiz.v@gmail.com>
2023-04-27 10:53:49 +0100

    fun. to construct opt. model & check templ. reci.

--- tests/optimisation_test/optimisation_manager.py
+++ tests/optimisation_test/optimisation_manager.py
@@ -3,17 +3,6 @@ from optimiser_base import OptimizerBase
 import pandas as pd
 from pathlib import Path
 import importlib
-from optimisation_model import OptimisationModel
-
-class State:
-    @classmethod
-    def from_dict(cls, state_dict: dict={}):
-        model = OptimisationModel()
-        model.optimiser_module = state_dict['module']
-        model.optimiser_class = state_dict['class']
-        model.optimiser_hyperparameters = state_dict['optimiser_hyperparameters']
-        model.save()
-        return cls(model)
     
 class OptimisationManager():
     
@@ -27,16 +16,24 @@ class OptimisationManager():
         #self.target_name = None  
         self.model = self._generate_model(pbounds=bound)
         self._probed_points = pd.DataFrame([])
+    
+    def update_model(self, data: pd.DataFrame, **kwargs):
+        """
+        Record probed points and update optimization model.
+        :param data:
 
-    def construct_optimiser_from_config_file(self, config_file_path:str):
-        return State.from_dict(self._config_dict['optimizer'])
+        """
+        self._probed_points = self._probed_points.append(data)
+        params, targets = self._pandas_to_params_targets(data)
+        for param, target in zip(params, targets):
+            self.model.register(param, target)
     
     def is_recipe_template_available(self, _template_path):
         _template_path = Path(_template_path)
         return _template_path.is_file()
 
 
-    #optimization_part
+    #internal functions
     
     def _generate_bound_from_config(self):
         constraint = {}
@@ -54,25 +51,13 @@ class OptimisationManager():
         optimizer_class = getattr(module, self._config_dict['optimizer']['class'])
         return optimizer_class(**self._config_dict['optimizer']['hyperparameters'], **kwargs)
 
-
-    def update_model(self, data: pd.DataFrame, **kwargs):
-        """
-        Record probed points and update optimization model.
-        :param data:
-
-        """
-        self._probed_points = self._probed_points.append(data)
-        params, targets = self.pandas_to_params_targets(data)
-        for param, target in zip(params, targets):
-            self.model.register(param, target)
-
-    def pandas_to_params_targets(self, data: pd.DataFrame):
+    def _pandas_to_params_targets(self, data: pd.DataFrame):
         """
         Split experiment result dataframe into dictionary format params and target array.
         """
         # get optimization target name
         if self.target_name is None:
-            target_name = [i for i in data.columns if i not in self.component_keys]
+            target_name = [i for i in data.columns if i not in self._component_keys]
             assert len(target_name) == 1, 'Require only one unseen column as optimization target'
             self.target_name = target_name[0]
         else:
@@ -86,12 +71,11 @@ class OptimisationManager():
             params.append(data.iloc[row_index].to_dict())
         return params, targets
 
-    def generate_batch(self, **kwargs) -> pd.DataFrame:
-        pass
-
-
 if __name__ == "main":
     test_data = pd.DataFrame([[3,4], [1, 2]], columns=['x', 'y'])
     opt = OptimisationManager('test_config.yaml')
     opt.update_model(test_data)
+    cwd_path = Path.cwd()
+    path_to_template_recipe = Path.joinpath(cwd_path, "tests/optimisation_test/algae_bot_recipe_template.yaml")
+    opt.is_recipe_template_available(path_to_template_recipe)
 
