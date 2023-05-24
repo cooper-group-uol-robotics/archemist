77a5b1a64699328f3d2598c9f3d30feaa794388a
Satheeshkumar <sathiz.v@gmail.com>
2023-05-24 09:39:00 +0100

    station created yet to be tested

---
+++ src/archemist/stations/filtration_station/process.py
@@ -0,0 +1,101 @@
+
+from typing import Dict
+from datetime import datetime, timedelta
+from transitions import State
+from archemist.core.state.station import Station
+from archemist.core.state.robot import RobotTaskType
+from archemist.robots.kmriiwa_robot.state import KukaLBRTask, KukaLBRMaintenanceTask, KukaNAVTask
+from .state import FiltrationStation, FiltrationValveOpenOpDescriptor, FiltrationValveCloseOpDescriptor
+from archemist.core.persistence.object_factory import StationFactory
+from archemist.core.processing.station_process_fsm import StationProcessFSM
+from archemist.core.persistence.object_factory import StationFactory
+from archemist.core.util import Location
+
+class FiltrationStationSm(StationProcessFSM):
+    
+    def __init__(self, station: Station, params_dict: Dict):
+        super().__init__(station, params_dict)
+        if 'operation_complete' not in self._status.keys():
+            self._status['operation_complete'] = False
+        self._status['filteration_complete'] = False
+
+        ''' States '''
+        states = [ State(name='init_state'), 
+            State(name='disable_auto_functions', on_enter=['request_disable_auto_functions']),
+            State(name='enable_auto_functions', on_enter=['request_enable_auto_functions']),
+            State(name='filteration_process', on_enter=['request_process_operation']),
+            State(name='navigate_to_filtration_station', on_enter=['request_navigate_to_filtration']),
+            State(name='open_drain_valve', on_enter=['request_open_drain_valve']),
+            State(name='close_drain_valve', on_enter=['request_close_drain_valve']),
+            State(name='pickup_funnel', on_enter=['request_pickup_funnel']),
+            State(name='final_state', on_enter=['finalize_batch_processing'])]
+
+        ''' Transitions '''
+        transitions = [
+            {'trigger':self._trigger_function, 'source':'init_state', 'dest': 'open_drain_valve', 'conditions':'all_batches_assigned'},
+            {'trigger':self._trigger_function, 'source':'open_drain_valve','dest':'draining_process', 'conditions':'is_station_job_ready'},
+            {'trigger':self._trigger_function, 'source':'draining_process','dest':'close_drain_valve', 'conditions':['is_station_job_ready','is_draining_process_complete']},
+            {'trigger':self._trigger_function, 'source':'close_drain_valve','dest':'disable_auto_functions', 'conditions':'is_station_job_ready'},
+            {'trigger':self._trigger_function, 'source':'disable_auto_functions','dest':'navigate_to_filtration_station', 'conditions':'is_station_job_ready'},
+            {'trigger':self._trigger_function, 'source':'navigate_to_filtration_station','dest':'pickup_funnel', 'conditions':'is_station_job_ready'},
+            {'trigger':self._trigger_function, 'source':'pickup_funnel','dest':'enable_auto_functions', 'conditions':'is_station_job_ready'},
+            {'trigger':self._trigger_function, 'source':'enable_auto_functions','dest':'final_state', 'conditions':['is_station_job_ready', 'is_station_operation_complete']}
+        ]   
+
+        self.init_state_machine(states=states, transitions=transitions)
+
+    def is_station_operation_complete(self):
+        return self._status['operation_complete']
+    
+    def request_process_operation (self):
+
+        # Add timer
+        # target_duration = kwargs['duration']
+        # start_time = rospy.get_time()
+        # elepsed_duration = 0
+        # while elepsed_duration < target_duration:
+        #     rospy.sleep(1)
+        #     elepsed_duration = rospy.get_time() - start_time
+        self._status['filteration_complete'] = True
+
+
+    def request_open_drain_valve(self):
+        self._station.assign_station_op(FiltrationValveOpenOpDescriptor.from_args())
+
+
+    def request_close_drain_valve(self):
+        self._station.assign_station_op(FiltrationValveCloseOpDescriptor.from_args())
+
+    def is_draining_process_complete(self):
+        return self._status['draining_complete']
+
+    def request_pickup_funnel(self):
+        robot_job = KukaLBRTask.from_args(name='UnloadWaitingStation',params=[False,self._status['batch_index']+1],
+                                type=RobotTaskType.LOAD_TO_ROBOT, location=self._station.location)
+        current_batch_id = self._station.assigned_batches[self._status['batch_index']].id
+        self._station.request_robot_op(robot_job,current_batch_id)
+
+    def request_navigate_to_filtration(self):
+        self._station.request_robot_op(KukaNAVTask.from_args(Location(26,1,''), False)) #TODO get this property from the config
+
+
+    def request_disable_auto_functions(self):
+        self._station.request_robot_op(KukaLBRMaintenanceTask.from_args('DiableAutoFunctions',[False]))
+
+    def request_enable_auto_functions(self):
+        self._station.request_robot_op(KukaLBRMaintenanceTask.from_args('EnableAutoFunctions',[False]))
+
+    def request_process_operation(self):
+        current_op = self._station.assigned_batches[-1].recipe.get_current_task_op()
+        self._station.assign_station_op(current_op)
+
+    def finalize_batch_processing(self):
+        self._station.process_assigned_batches()
+        self._status['filteration_complete'] = False
+        self._status['operation_complete'] = False
+        self._status['batch_index'] = 0
+        self._status['batches_count'] = 0
+        self.to_init_state()
+
+
+
