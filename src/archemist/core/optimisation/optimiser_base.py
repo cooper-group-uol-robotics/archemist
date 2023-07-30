312f3dc24e2aea9b016ea31b6fd60c68a933c054
stoic-roboticist <hatem.fakhruldeen@gmail.com>
2023-07-30 21:02:43 +0100

    added optimiser and its tests

---
+++ src/archemist/core/optimisation/optimiser_base.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import abc
+
+
+class OptimiserBase(abc.ABC):
+    def __init__(self,
+                 **kwargs) -> None:
+
+        self.kwargs = kwargs
+
+    @abc.abstractmethod
+    def generate_batch(self, **kwargs) -> pd.DataFrame:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def update_model(self, data: pd.DataFrame, **kwargs):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def _generate_model(self, **kwargs):
+        """
+        Generate optimization model from config file based on the 'optimizer' part.
+        If the model requires constraint information, please pass it via kwargs.
+        Overwrite this method to manually generate optimization model. If this method is not overwritten, module
+        information must be passed through config file.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def _generate_bound_from_config(self, **kwargs):
+        raise NotImplementedError
