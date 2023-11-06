962c07f5ad02982a2b2884410f41d4c6d7c22df7
stoic-roboticist <hatem.fakhruldeen@gmail.com>
2023-11-06 16:43:15 +0000

    fixed bugs with robots

--- src/archemist/robots/kmriiwa_robot/state.py
+++ src/archemist/robots/kmriiwa_robot/state.py
@@ -6,10 +6,3 @@ from typing import Union
 class KMRIIWARobot(MobileRobot):
     def __init__(self, robot_model: Union[MobileRobotModel, ModelProxy]) -> None:
         super().__init__(robot_model)
-
-    @classmethod
-    def from_dict(cls, robot_document: dict):
-        model = MobileRobotModel()
-        cls._set_model_common_fields(model, robot_document)
-        model.save()
-        return cls(model)
--- src/archemist/robots/yumi_robot/state.py
+++ src/archemist/robots/yumi_robot/state.py
@@ -1,15 +1,8 @@
 from archemist.core.persistence.models_proxy import ModelProxy
 from archemist.core.models.robot_model import RobotModel
-from archemist.core.state.robot import Robot
+from archemist.core.state.robot import FixedRobot
 from typing import Union
 
-class YuMiRobot(Robot):
+class YuMiRobot(FixedRobot):
     def __init__(self, robot_model: Union[RobotModel, ModelProxy]) -> None:
         super().__init__(robot_model)
-
-    @classmethod
-    def from_dict(cls, robot_document: dict):
-        model = RobotModel()
-        cls._set_model_common_fields(model, robot_document)
-        model.save()
-        return cls(model)
