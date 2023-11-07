edabc898b286f5cd415a377007a97f84a6ce74fe
stoic-roboticist <hatem.fakhruldeen@gmail.com>
2023-11-07 12:03:10 +0000

    added solubility process to peristaltic pumps station

---
+++ src/archemist/stations/peristaltic_pumps_station/process.py
@@ -0,0 +1,64 @@
+from transitions import State
+from archemist.core.persistence.models_proxy import ModelProxy
+from archemist.core.state.lot import Lot
+from .state import PeristalticPumpsStation
+from archemist.core.state.robot_op import RobotTaskOpDescriptor
+from archemist.core.state.station_process import StationProcess, StationProcessModel
+from typing import List, Dict, Any
+
+from typing import Union
+
+class PandaPumpSolubilityProcess(StationProcess):
+    def __init__(self, process_model: Union[StationProcessModel, ModelProxy]) -> None:
+        super().__init__(process_model)
+        
+        ''' States '''
+        self.STATES = [ State(name='init_state'),
+            State(name='prep_state'),
+            State(name='move_dispense_head', on_enter='request_move_dispense_head'),
+            State(name='dispense_liquid', on_enter='request_dispense_liquid'),
+            State(name='replace_dispense_head', on_enter='request_replace_dispense_head'),
+            State(name='final_state')]
+
+        ''' Transitions '''
+        self.TRANSITIONS = [
+            {'source':'init_state', 'dest': 'prep_state'},
+            {'source':'prep_state','dest':'move_dispense_head'},
+            {'source':'move_dispense_head','dest':'dispense_liquid', 'conditions':'are_req_robot_ops_completed'},
+            {'source':'dispense_liquid','dest':'replace_dispense_head', 'conditions':'are_req_station_ops_completed'},
+            {'source':'replace_dispense_head','dest':'final_state', 'conditions':'are_req_robot_ops_completed'}
+        ]
+
+    @classmethod
+    def from_args(cls, lot: Lot,
+                  operations: List[Dict[str, Any]] = None,
+                  skip_robot_ops: bool=False,
+                  skip_station_ops: bool=False,
+                  skip_ext_procs: bool=False
+                  ):
+        model = StationProcessModel()
+        cls._set_model_common_fields(model,
+                                     PeristalticPumpsStation.__name__,
+                                     lot,
+                                     operations,
+                                     skip_robot_ops,
+                                     skip_station_ops,
+                                     skip_ext_procs)
+        model.save()
+        return cls(model)
+
+    ''' states callbacks '''
+
+    def request_move_dispense_head(self):
+        robot_op = RobotTaskOpDescriptor.from_args(name='move_dispense_head', target_robot="PandaRobot")
+        
+        self.request_robot_ops([robot_op])
+
+    def request_dispense_liquid(self):
+        sample = self.lot.batches[0].samples[0]
+        current_op = self.generate_operation("dispense_op", target_sample=sample)
+        self.request_station_op(current_op)
+
+    def request_replace_dispense_head(self):
+        robot_op = RobotTaskOpDescriptor.from_args(name='replace_dispense_head', target_robot="PandaRobot")
+        self.request_robot_ops([robot_op])
--- tests/stations/test_peristaltic_pumps_station.py
+++ tests/stations/test_peristaltic_pumps_station.py
@@ -5,10 +5,13 @@ from mongoengine import connect
 
 from archemist.stations.peristaltic_pumps_station.state import (PeristalticPumpsStation,
                                                                 PPLiquidDispenseOp)
-from archemist.core.util.enums import StationState, OpOutcome
+from archemist.core.util.enums import StationState, OpOutcome, ProcessStatus
 from archemist.stations.peristaltic_pumps_station.handler import SimPeristalticPumpsStationHandler
+from archemist.stations.peristaltic_pumps_station.process import PandaPumpSolubilityProcess
+from archemist.core.state.robot_op import RobotTaskOpDescriptor
 from archemist.core.state.lot import Lot, Batch
 from archemist.core.state.station_op_result import MaterialOpResult
+from .testing_utils import test_req_robot_ops, test_req_station_op
 
 class PeristalticLiquidDispensingTest(unittest.TestCase):
     def setUp(self):
@@ -16,7 +19,7 @@ class PeristalticLiquidDispensingTest(unittest.TestCase):
         self._client = connect(db=self._db_name, host='mongodb://localhost:27017', alias='archemist_state')
 
         station_dict = {
-            'type': 'PeristalticLiquidDispensing',
+            'type': 'PeristalticPumpsStation',
             'id': 20,
             'location': {'coordinates': [1,7], 'descriptor': "ChemSpeedFlexStation"},
             'total_lot_capacity': 1,
@@ -79,6 +82,59 @@ class PeristalticLiquidDispensingTest(unittest.TestCase):
         self.station.complete_assigned_op(OpOutcome.SUCCEEDED, [op_result])
         self.assertEqual(self.station.liquids_dict["water"].volume, 300)
         self.assertEqual(self.station.liquids_dict["water"].volume_unit, "mL")
+
+    def test_solubility_process(self):
+        
+        batch = Batch.from_args(2)
+        lot = Lot.from_args([batch])
+        
+        # add batches to station
+        self.station.add_lot(lot)
+
+        # create station process
+        operations = [
+                {
+                    "name": "dispense_op",
+                    "op": "PPLiquidDispenseOp",
+                    "parameters": {
+                        "liquid_name": "water",
+                        "dispense_volume": 10,
+                        "dispense_unit": "mL"
+                    }
+                }
+            ]
+        process = PandaPumpSolubilityProcess.from_args(lot=lot,
+                                                operations=operations)
+        process.lot_slot = 0
+
+        # assert initial state
+        self.assertEqual(process.m_state, 'init_state')
+        self.assertEqual(process.status, ProcessStatus.INACTIVE)
+
+        # prep_state
+        process.tick()
+        self.assertEqual(process.status, ProcessStatus.RUNNING)
+        self.assertEqual(process.m_state, 'prep_state')
+
+        # move_dispense_head
+        process.tick()
+        self.assertEqual(process.m_state, 'move_dispense_head')
+        test_req_robot_ops(self, process, [RobotTaskOpDescriptor])
+
+        # dispense_liquid
+        process.tick()
+        self.assertEqual(process.m_state, 'dispense_liquid')
+        test_req_station_op(self, process, PPLiquidDispenseOp)
+
+        # replace_dispense_head
+        process.tick()
+        self.assertEqual(process.m_state, 'replace_dispense_head')
+        test_req_robot_ops(self, process, [RobotTaskOpDescriptor])
+
+        # final_state
+        process.tick()
+        self.assertEqual(process.m_state, 'final_state')
+        self.assertEqual(process.status, ProcessStatus.FINISHED)
 
     def test_sim_handler(self):
         batch_1 = Batch.from_args(3)
